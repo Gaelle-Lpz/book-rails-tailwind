7c5165c29b228aa710be4456250a72d14aa0e504
Gaelle Lopez <lope.gaelle@gmail.com>
2023-08-30 18:49:51 +0200

    add model book

---
+++ db/migrate/20230830164757_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.text :description
+      t.string :author
+      t.integer :score
+      t.string :cover_img_url
+      t.string :isbn
+
+      t.timestamps
+    end
+  end
+end
