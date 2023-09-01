4686d372241bd480aa7b2b450ab09c8f12885f96
Gaelle Lopez <lope.gaelle@gmail.com>
2023-09-01 13:53:59 +0200

    navbar v1 and darkmode

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem "jbuilder"
 # Authentication
 gem "devise"
 
+# Fontawesome
+gem "font-awesome-sass", "~> 6.4.2"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,5 @@ class Book < ApplicationRecord
   has_many :users, through: :user_books
   has_many :reviews
 
-  #enum :status, { wished: 0, pending: 1, read: 2, archive: 3 }
+  # enum :status, { wished: 0, pending: 1, read: 2, archive: 3 }
 end
