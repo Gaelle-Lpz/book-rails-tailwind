4dddc062bdd5cb786777351df945472891a34ecd
Gaelle Lopez <lope.gaelle@gmail.com>
2023-09-03 19:56:54 +0200

    add front review to show book

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,4 +6,13 @@ class Book < ApplicationRecord
   # enum :status, { wished: 0, pending: 1, read: 2, archive: 3 }
 
   validates :isbn, uniqueness: true
+
+  def average_rating
+    total_rating = reviews.sum { |review| review.rating.to_f }
+    valid_reviews_count = reviews.count { |review| !review.rating.nil? }
+
+    return total_rating / valid_reviews_count if valid_reviews_count.positive?
+
+    0.0
+  end
 end
